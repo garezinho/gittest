1e5b693ac2f4925a7d76c64e22b3a8c55a0a7f54
Guilherme Gare <gui10.gare@gmail.com>
2023-12-05 15:20:17 -0300

    js changed 4.0

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let oi = 0; 
+let oi = 0;
 
-while(oi == 0){
+while (oi != 10) {
     oi++
+
+    if (oi > 0) {
+        console.log("Hello man");
+    }
+    else {
+        console.log("Fuck you ma");
+    }
 }
 
-if(oi > 0){
-    console.log("Hello man");
-}
-else {
-    console.log("Fuck you ma");
-}
+
+
